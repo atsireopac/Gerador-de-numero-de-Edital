8e0c67a8339151a98eff8bab5a55d8715cdd56ef
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2024-04-30 11:49:37 -0300

    acrescentei estilização a página

--- gerador-numero-edital.js
+++ gerador-numero-edital.js
@@ -1,7 +1,7 @@
 
 /*let numeroOrdem = document.querySelector("input#numeroOrdem");
 let anoCadastro = document.querySelector("input#anoCadastro");
-let resultado = document.querySelector("div#resultado");*/
+let resultado = document.querySelector("div#resultado");
 
 
 function gerarNumeroEdital(numeroOrdem, anoCadastro) {
@@ -13,4 +13,4 @@ function gerarNumeroEdital(numeroOrdem, anoCadastro) {
 
 const numeroEdital = gerarNumeroEdital(24, 2024);
 console.log(numeroEdital);
-
+*/
