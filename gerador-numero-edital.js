9ea1e6a46668db08fe29682a9234fec97b6f790e
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2024-04-30 11:24:38 -0300

    atualização

--- gerador-numero-edital.js
+++ gerador-numero-edital.js
@@ -1,5 +1,9 @@
-const XLSX = require('xlsx');
+//const XLSX = require('xlsx');
+let numeroOrdem = document.querySelector("input#numeroOrdem");
+let anoCadastro = document.querySelector("input#anoCadastro");
+let resultado = document.querySelector("div#resultado");
 
+/*
 // Função para verificar se o número de edital já existe no banco de dados
 function verificarExistenciaEdital(numeroEdital) {
     // Carregar o arquivo Excel
@@ -21,35 +25,35 @@ function verificarExistenciaEdital(numeroEdital) {
 
     return false; // O número de edital não existe
 }
-
+*/
 // Função para gerar o número de acompanhamento do edital
 function gerarNumeroEdital(numeroOrdem, anoCadastro) {
     // Verifica se o número de ordem e o ano de cadastro foram fornecidos
-    if (!numeroOrdem || !anoCadastro) {
-        return "Número de ordem e ano de cadastro são obrigatórios.";
-    }
-
+    //if (!numeroOrdem || !anoCadastro) {
+        //window.alert('Número de ordem e ano de cadastro são obrigatórios.');
+    //}
     // Verifica se o número de ordem é um número válido
-    if (isNaN(numeroOrdem)) {
-        return "O número de ordem deve ser um número válido.";
-    }
+   /* if (isNaN(numeroOrdem)) {
+        window.alert('O número de ordem deve ser um número válido.');
+    }*/
 
     // Verifica se o ano de cadastro é um número válido e tem quatro dígitos
-    if (isNaN(anoCadastro) || anoCadastro.toString().length !== 4) {
-        return "O ano de cadastro deve ser um número válido com quatro dígitos.";
-    }
+    /*if (isNaN(anoCadastro) || anoCadastro.toString().length >= 4) {
+        window.alert('O ano de cadastro deve ser um número válido com quatro dígitos.');
+    }*/
 
     // Formata o número de ordem e o ano de cadastro no formato desejado
-    const numeroEdital = numeroOrdem + "/" + anoCadastro;
+    //let numeroEdital = numeroOrdem + "/" + anoCadastro;
 
     // Verifica se o número de edital já existe no banco de dados
-    if (verificarExistenciaEdital(numeroEdital)) {
+   /* if (verificarExistenciaEdital(numeroEdital)) {
         return "O número de edital já existe no banco de dados.";
-    }
+    }*/
 
-    return numeroEdital;
+    resultado.innerHTML += `${numeroEdital}`;
+    
 }
 
 // Exemplo de uso da função para gerar um número de acompanhamento de edital
-var numeroEdital = gerarNumeroEdital(51, 2024);
-console.log(numeroEdital); // Saída: 51/2024
+//var numeroEdital = gerarNumeroEdital(51, 2024);
+//console.log(numeroEdital); // Saída: 51/2024
